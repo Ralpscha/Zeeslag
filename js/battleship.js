215f848fe36aeb4e95224b12ef786a5833fce62a
Ralph van Schaik <ralph.van.schaik@me.com>
2020-12-22 14:24:02 +0100

    344 model incl hit assessment

--- js/battleship.js
+++ js/battleship.js
@@ -13,8 +13,36 @@ var view = {
   }
 }
 
-view.displayMiss("00");
-view.displayHit("34");
-view.displayMiss("55");
-view.displayHit("12");
-view.displayMessage("Tap tap, is this thing on?");
+var model = {
+  boardSize : 7,
+  numShips : 3,
+  shipLength : 3,
+  shipsSunk : 0,
+
+  ships : [
+    {locations: ["10","20","30"], hits: ["", "", ""]},
+    {locations: ["32","33","34"], hits: ["", "", ""]},
+    {locations: ["63","64","65"], hits: ["", "", "hit"]}
+  ],
+
+  fire : function (guess) {
+    for (var = i; i < this.numShips; i++) {
+      var ship = this.ships[i];
+      var locations = ship.locations;
+      var index = locations.indexOf(guess);
+      if (index >= 0) {
+      //  we have a hit!
+        ship.hits[index] = "hit";
+        return true;
+      }
+    }
+    return false;
+  }
+
+
+};
+
+
+
+
+
