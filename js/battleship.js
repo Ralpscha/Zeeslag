77e8578584223f56f95a6673ca8c5834d98a6a69
Ralph van Schaik <ralph.van.schaik@me.com>
2020-12-24 11:57:03 +0100

    372 testlines verwijderd

--- js/battleship.js
+++ js/battleship.js
@@ -108,17 +108,6 @@ var model = {
   }
 };
 
-// console.log(model.fire('10'));
-// model.fire("53");
-// model.fire('06');
-// model.fire('16');
-// model.fire('26');
-// model.fire('34');
-// model.fire('24');
-// model.fire('44');
-// model.fire('12');
-// model.fire('11');
-// model.fire('10');
 
 var controller = {
   guesses: 0,
@@ -157,22 +146,6 @@ function parseGuess(guess) {
   return null;
 }
 
-// console.log(parseGuess("A0"));
-// console.log(parseGuess("B6"));
-// console.log(parseGuess("G3"));
-// console.log(parseGuess("H0"));
-// console.log(parseGuess("A7"));
-
-// controller.processGuess("A0");
-// controller.processGuess("A6");
-// controller.processGuess("B6");
-// controller.processGuess("C6");
-// controller.processGuess("C4");
-// controller.processGuess("D4");
-// controller.processGuess("E4");
-// controller.processGuess("B0");
-// controller.processGuess("B1");
-// controller.processGuess("B2");
 
 function handleFireButton() {
   var guessInput = document.getElementById('guessInput');
